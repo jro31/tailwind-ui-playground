338cd6d89c70bdcb97614336474bd95c3ff6a0b3
Jethro Williams <jethrowilliams@outlook.com>
2022-03-21 15:52:44 +0000

    Link to homepage from nav logo

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
-/* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
 
@@ -34,18 +34,20 @@ const Navbar = () => {
                 </Disclosure.Button>
               </div>
               <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
-                <div className='flex-shrink-0 flex items-center'>
-                  <img
-                    className='block lg:hidden h-8 w-auto'
-                    src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
-                    alt='Workflow'
-                  />
-                  <img
-                    className='hidden lg:block h-8 w-auto'
-                    src='https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg'
-                    alt='Workflow'
-                  />
-                </div>
+                <Link to='/'>
+                  <div className='flex-shrink-0 flex items-center'>
+                    <img
+                      className='block lg:hidden h-8 w-auto'
+                      src='https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg'
+                      alt='Workflow'
+                    />
+                    <img
+                      className='hidden lg:block h-8 w-auto'
+                      src='https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg'
+                      alt='Workflow'
+                    />
+                  </div>
+                </Link>
                 {/* Desktop menu */}
                 <div className='hidden sm:block sm:ml-6'>
                   <div className='flex space-x-4'>
